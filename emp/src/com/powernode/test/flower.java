51c2789cd52d8763145b6da682a1cbcc5233c7cc
summer <957041293@qq.com>
2021-08-22 12:58:29 +0800

    add flower.java

--- emp/src/com/powernode/test/flower.java
+++
@@ -1,7 +0,0 @@
-package src.com.powernode.test;
-
-public class flower {
-    public static void main(String[] args) {
-        System.out.println("maybe you will think of me,like a flower never be bloom again");
-    }
-}
